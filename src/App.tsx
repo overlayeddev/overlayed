2f9835d43683d6b3fc13fa150c92b6e144353ed0
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-11-03 22:20:05 -0500

    Goodish lookin

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { useAppStore } from "./store";
 import socket from "./rpc/manager";
 import { useEffect } from "react";
 import { appWindow } from "@tauri-apps/api/window";
+import { User } from "./user";
 
 function App() {
-  const { me, users, currentChannel, clearUsers} = useAppStore();
+  const { users } = useAppStore();
 
   useEffect(() => {
     socket.init();
@@ -21,20 +22,10 @@ function App() {
       </div>
       <div className="py-2">
         {Object.entries(users).map(([_k, item]) => (
-          <div key={item.id} className="flex py-1 items-center">
-            <div
-              className="rounded-full bg-black w-8 h-8 border-2 border-slate-800 mr-2"
-              style={{
-                borderColor: item.talking ? "limegreen" : "inherit",
-              }}
-            />
-
-            <div className="text-white">{item.username}</div>
-          </div>
+          <User key={item.id} item={item} />
         ))}
       </div>
-      <button className="bg-red-500" onClick={() => clearUsers()}>Reset</button>
-      <pre className="text-white">{JSON.stringify({ currentChannel }, null, 2)}</pre>
+      {/* <pre className="text-white">{JSON.stringify({ currentChannel }, null, 2)}</pre> */}
     </div>
   );
 }
---
+++ src/user.tsx
@@ -0,0 +1,23 @@
+import { OverlayedUser } from "./types";
+
+export const User = ({ item }: { item: OverlayedUser }) => {
+  const { id, avatarHash } = item;
+
+  const avatarUrl = avatarHash
+    ? `https://cdn.discordapp.com/avatars/${id}/${avatarHash}.jpg`
+    : "./img/default.png";
+
+  const talkingClass = item.talking ? "border-green-500" : "border-slate-600";
+
+  return (
+    <div className="flex py-1 items-center">
+      <div
+        className={`rounded-full bg-black w-8 h-8 border-2 border-slate-800 mr-2 ${talkingClass}`}
+      >
+        <img src={avatarUrl} alt="avatar" className="rounded-full" />
+      </div>
+
+      <div className="text-white rounded-md bg-zinc-900 p-1">{item.username}</div>
+    </div>
+  );
+};
