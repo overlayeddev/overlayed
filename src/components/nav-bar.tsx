99051cb386647c6b1fd2067fa0cb380faa2aaa71
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-11-05 11:31:01 -0600

    Add clickthrough controls/ui

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,9 +1,26 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Settings, Home } from "lucide-react";
+import { Settings, Home, Eye, EyeOff } from "lucide-react";
+
+import { listen } from "@tauri-apps/api/event";
+import { invoke } from "@tauri-apps/api";
 
 export const NavBar = () => {
   const location = useLocation();
+  const [clickthrough, setClickthrough] = useState(false);
+
+  useEffect(() => {
+    const unlisten = listen < boolean > ("toggle_clickthrough", (event) => {
+      // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
+      // event.payload is the payload object
+      console.log(event);
+      setClickthrough(event.payload);
+    });
+
+    return () => {
+      unlisten.then((f) => f());
+    };
+  }, []);
 
   const getNavLink = useCallback(() => {
     if (["/channel", "/"].includes(location.pathname)) {
@@ -27,13 +44,27 @@ export const NavBar = () => {
     }
   }, [location.pathname]);
 
+  if (clickthrough && location.pathname === "/channel") return null;
+
   return (
     <div
       data-tauri-drag-region
       className="cursor-default rounded-t-lg font-bold select-none p-2 bg-zinc-900"
     >
       overlayed
-      <div className="float-right">{getNavLink()}</div>
+      <div className="float-right flex gap-2">
+        <div>
+          <button>
+            <Eye
+              onClick={() => {
+                setClickthrough(!clickthrough);
+                invoke("toggle_clickthrough", { enabled: !clickthrough });
+              }}
+            />
+          </button>
+        </div>
+        <div>{getNavLink()}</div>
+      </div>
     </div>
   );
 };
--- src/views/settings.tsx
+++ src/views/settings.tsx
@@ -1,27 +1,8 @@
-import { useAppStore } from "../store";
-import { invoke } from "@tauri-apps/api/tauri";
-
 export const Settings = () => {
-  const { setClickThrough, clickThrough } = useAppStore();
 
   return (
     <div className="h-screen pt-2 bg-zinc-800 p-2">
       <h1 className="text-xl pl-2 pt-2 font-bold">Settings</h1>
-      <button
-        className="bg-blue-800 p-2 rounded-md"
-        onClick={() => {
-          const updatedClickThrough = !clickThrough;
-
-          setClickThrough(updatedClickThrough);
-
-          invoke("toggle_clickthrough", {
-            enabled: updatedClickThrough,
-          });
-
-        }}
-      >
-        {clickThrough ? "Disable" : "Enable"} clickThrough
-      </button>
       <div className="pt-2 pl-2">
         <button
           onClick={() => {
