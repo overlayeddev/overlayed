a50d59fb29934371856d3ecdb47c53bd7121522a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-11-21 21:00:09 -0600

    Build all

--- scripts/windows.ts
+++ scripts/windows.ts
@@ -39,13 +39,7 @@ const BINS: Record<AllowedPlatforms, BinType> = {
 export const signWindowsBinary = async () => {
   const { bundle, target } = BINS.win32;
   const binBasePath = `${BASE_PATH}/target/${target}/release/bundle/${bundle}`;
-
-  // CI path:   apps\desktop\src-tauri\target\x86_64-pc-windows-msvc\release\bundle\nsis\overlayed_0.0.0_x64-setup.exe
-  // CI input:  apps/desktop/src-tauri/target/x86_64-pc-windows-msvc/release/bundle/nsis
-
   const globPath = `${binBasePath}/*.exe`;
-  console.log("base path:", binBasePath);
-  console.log("glob path:", globPath);
 
   const [foundBinary] = await glob(globPath);
 
