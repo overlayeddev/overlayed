8d90fa943da06bddb824539f3365c5b313ddac43
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-11-22 15:52:51 -0600

    Test esm

---
+++ scripts/actions/sign.js
@@ -0,0 +1,50 @@
+import fs from "fs";
+const releaseId = "${{ needs.create-release.outputs.release_id }}";
+
+const SIGNED_BINARIES_DIR = "./signed/binaries";
+export const script = async() => {
+  if(!fs.existsSync(SIGNED_BINARIES_DIR)) {
+    console.log("No signed binaries found");
+    return;
+  }
+   
+  const files = fs.readdirSync(SIGNED_BINARIES_DIR);
+
+  const { data } = await github.rest.repos.listReleases({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  const [latestRelease] = data;
+
+  for (const asset of latestRelease?.assets ?? []) {
+    // skip if the asset doesnt end with msi or exe
+    if (![".msi", ".exe"].some((ext) => asset.name.endsWith(ext))) {
+      continue;
+    }
+
+    console.log("deleting asset", asset.name, asset.id);
+    await github.rest.repos.deleteReleaseAsset({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      release_id: releaseId,
+      asset_id: asset.id,
+    });
+  }
+
+  for (const file of files) {
+    const filePath = `${SIGNED_BINARIES_DIR}/${file}`;
+    const fileData = fs.readFileSync(filePath);
+
+    const { data: uploadResponse } = await github.rest.repos.uploadReleaseAsset({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      release_id: releaseId,
+      // @ts-ignore
+      data: fileData,
+      name: file,
+    });
+
+    console.log(uploadResponse);
+  }
+}
