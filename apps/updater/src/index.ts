e899ef366eb7108510362dae552d290cc07c723c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-11-29 09:10:19 -0600

    Update updater

--- apps/updater/src/index.ts
+++ apps/updater/src/index.ts
@@ -1,27 +1,72 @@
 import { Hono } from "hono/quick";
+import type { Endpoints } from "@octokit/types";
 
-type Bindings = {};
+type ReleaseResponse =
+	Endpoints["GET /repos/{owner}/{repo}/releases/latest"]["response"]["data"];
+
+type Bindings = {
+	GITHUB_TOKEN: string;
+};
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+const GIT_USER = "Hacksore";
+const GIT_REPO = "overlayed";
 
 app.get("/:target/:arch/:currentVersion", async (c) => {
 	const target = c.req.param("target");
 	const arch = c.req.param("arch");
 	const currentVersion = c.req.param("currentVersion");
 
-	const letter = "test";
+	console.log(`Fetching releases for ${target}/${arch}`);
 
-	return c.body(
-		JSON.stringify({
-			target,
-			arch,
-			currentVersion,
-		}),
-		200,
-		{
+	// fetch all releases from github
+	try {
+		const releases: ReleaseResponse = await fetch(
+			`https://api.github.com/repos/${GIT_USER}/${GIT_REPO}/releases/latest`,
+			{
+				headers: {
+					Authorization: `token ${c.env.GITHUB_TOKEN}`,
+					"User-Agent": "overlayed-updater v1",
+				},
+			},
+		).then((res) => res.json());
+
+		// get the assets that is called latest.json
+		const latest = releases.assets.find((asset) => asset.name === "latest.json");
+
+		if (!latest) {
+			return c.body(
+				JSON.stringify({ error: "Failed to find latest.json" }),
+				204,
+
+				{
+					"Content-Type": "application/json",
+				},
+			);
+		}
+
+		// download the latest.json and return it's response
+		const latestVersion = await fetch(latest.browser_download_url).then((res) =>
+			res.json(),
+		);
+
+
+		return c.body(JSON.stringify({ currentVersion, latestVersion }), 200, {
 			"Content-Type": "application/json",
-		},
-	);
+		});
+
+	} catch (err) {
+		console.log(err);
+		return c.body(
+			JSON.stringify({ error: "Failed to fetch releases" }),
+			500,
+
+			{
+				"Content-Type": "application/json",
+			},
+		);
+	}
 });
 
 export default app;
