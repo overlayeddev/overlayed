c55266a34cf2c1a9c971d50e1924b93fce54bcc8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-16 10:45:30 -0600

    "SEO"

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "Overlayed";
+export const SITE_TITLE = "Overlayed - Discord overlay for macOS, Windows, and Linux";
 export const SITE_DESCRIPTION =
-  "A modern, open-source, and free voice chat overlay for Discord. Ovlerayed supports Mac, Windows, and Linux!";
+  "A modern, open-source, and free voice chat overlay for Discord. Overlayed supports macOS, Windows, and Linux!";
 export const SITE_IMAGE = "/hero.png";
