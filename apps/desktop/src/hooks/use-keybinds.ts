652290ae8a168094a78d666a1fee821cdb878a00
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-17 12:04:58 -0600

    Add keybind for clickthrough

--- apps/desktop/src/hooks/use-keybinds.ts
+++ apps/desktop/src/hooks/use-keybinds.ts
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { register, isRegistered, unregister } from "@tauri-apps/api/globalShortcut";
 import { useAppStore } from "@/store";
+import { invoke } from "@tauri-apps/api";
 
-// TODO: make this configurable?
+// TODO: make these configurable?
 const HIDE_TOGGLE_KEYBIND = "Command+Shift+G";
+const TOGGLE_CLICKTHROUGH_KEYBIND = "Command+Shift+H";
+
 export const useKeybinds = () => {
   const { setAppVisible, visible } = useAppStore();
 
   useEffect(() => {
-    const registerKeybinds = async () => {
+    const registerKeybind = async () => {
       if (await isRegistered(HIDE_TOGGLE_KEYBIND)) {
         await unregister(HIDE_TOGGLE_KEYBIND);
       }
@@ -21,6 +24,20 @@ export const useKeybinds = () => {
       });
     };
 
-    registerKeybinds();
+    registerKeybind();
   }, [visible]);
+
+  useEffect(() => {
+    const registerKeybind = async () => {
+      if (await isRegistered(TOGGLE_CLICKTHROUGH_KEYBIND)) {
+        await unregister(TOGGLE_CLICKTHROUGH_KEYBIND);
+      }
+
+      await register(TOGGLE_CLICKTHROUGH_KEYBIND, () => {
+        invoke("toggle_clickthrough");
+      });
+    };
+
+    registerKeybind();
+  }, []);
 };
