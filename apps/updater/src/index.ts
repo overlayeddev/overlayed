27fef9a1b19f547614f1663b6de34795b583ec43
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-26 08:13:45 -0600

    Update updater

--- apps/updater/src/index.ts
+++ apps/updater/src/index.ts
@@ -31,13 +31,33 @@ app.get("/latest", async (c) => {
 });
 
 app.get("/:target/:arch/:currentVersion", async (c) => {
+	// TODO: updater metrics maybe?
 	const target = c.req.param("target");
 	const arch = c.req.param("arch");
 	const currentVersion = c.req.param("currentVersion");
 
-	return c.body(JSON.stringify({ error: "Failed to fetch releases" }), 204, {
-		"Content-Type": "application/json",
-	});
+	try {
+		const latestVersion = await getLatestVersions({
+			user: GIT_USER,
+			repo: GIT_REPO,
+			authToken: c.env.GITHUB_TOKEN,
+		});
+
+		if (currentVersion === latestVersion?.version) {
+			return c.body("", 204, {
+				"Content-Type": "application/json",
+			});
+		}
+
+		return c.body(JSON.stringify(latestVersion), 200, {
+			"Content-Type": "application/json",
+		});
+
+	} catch (e) {
+		return c.body(JSON.stringify(e), 500, {
+			"Content-Type": "application/json",
+		});
+	}
 });
 
 export default app;
