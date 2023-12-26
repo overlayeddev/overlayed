4e1f0c8c0655b0f2f9021b7e3e85261f8b0250e1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-26 08:02:49 -0600

    Cors on route

--- apps/updater/src/index.ts
+++ apps/updater/src/index.ts
@@ -11,7 +11,6 @@ const app = new Hono < { Bindings: Bindings } > ();
 const GIT_USER = "Hacksore";
 const GIT_REPO = "overlayed";
 
-app.use(cors());
 app.get("/latest", async (c) => {
 	return c.body(
 		JSON.stringify(
@@ -27,7 +26,7 @@ app.get("/latest", async (c) => {
 			"Content-Type": "application/json",
 		},
 	);
-});
+}, cors());
 
 app.get("/:target/:arch/:currentVersion", async (c) => {
 	const target = c.req.param("target");
