5f669a19eba5870efc305201db89dfa1008fee72
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-28 13:41:30 -0600

    Test no adapter

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
+// import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
@@ -19,9 +19,9 @@ export default defineConfig({
     tailwind(),
     mdx(),
   ],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  // adapter: vercel({
+  //   webAnalytics: {
+  //     enabled: true,
+  //   },
+  // }),
 });
