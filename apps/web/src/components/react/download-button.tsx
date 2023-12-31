5ed3061abcd6fb57ba058f8c32d3e8cc3c0f7ead
Nick C <niemal@users.noreply.github.com>
2023-12-31 22:15:24 +0200

    feat: add OS decoration with hover effect on download buttons (#16)
    Co-authored-by: Sean Boult <996134+Hacksore@users.noreply.github.com>

---
+++ apps/web/src/components/react/download-button.tsx
@@ -0,0 +1,37 @@
+import type { ComponentPropsWithoutRef } from "react";
+import { Platforms } from "./download";
+import type { PlatformDownload } from "types/types";
+
+type DownloadButtonParams = {
+  platform: PlatformDownload;
+  props?: ComponentPropsWithoutRef<"a">;
+};
+
+export default function DownloadButton({
+  platform,
+  ...props
+}: DownloadButtonParams) {
+  return (
+    <div
+      className="flex flex-col items-center justify-center gap-2 p-4 hover:bg-gray-700 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+      {...props}
+    >
+
+      <a
+        target="_blank"
+        href={platform.url}
+        className="flex flex-col items-center justify-center gap-2 w-20"
+      >
+        <img
+          src={`/${platform.platform}_inverse.svg`}
+          width={48}
+          height={48}
+          alt={`${platform.platform}'s image`}
+        />
+        <span className="platform-button-text font-bold z-30">
+          {Platforms[platform.platform]}
+        </span>
+      </a>
+    </div>
+  );
+}
--- apps/web/src/components/react/download.tsx
+++ apps/web/src/components/react/download.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 
 // TODO: fix imports
 import type { PlatformDownload } from "types/types";
+import DownloadButton from "./download-button";
 
-const Platforms = {
+export const Platforms = {
   linux: "Linux",
   windows: "Windows",
   mac: "macOS",
@@ -13,7 +14,9 @@ const API_HOST =
   process.env.NODE_ENV === "production" ? "https://update.overlayed.dev" : "";
 
 export const Download = () => {
-  const [platformDownloads, setPlatformDownloads] = useState<PlatformDownload[]>([]);
+  const [platformDownloads, setPlatformDownloads] = useState<
+    PlatformDownload[]
+  >([]);
 
   useEffect(() => {
     fetch(`${API_HOST}/latest`)
@@ -22,19 +25,15 @@ export const Download = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="text-3xl pb-8">Download</h2>
-      <div className="flex gap-2">
-        {platformDownloads.map(item => (
-          <a
-            key={item.platform}
-            target="_blank"
-            href={item.url}
-            className="hover:bg-primary bg-primary py-2 px-4 rounded-lg"
-          >
-            {Platforms[item.platform]}
-          </a>
-        ))}
+    <div className="relative w-full h-[100%] pb-20 overflow-hidden">
+      <div className="parent-download flex flex-col items-center w-max ml-auto mr-auto sm:w-full">
+        <h2 className="text-2xl pb-8">Download</h2>
+        <div className="flex gap-6 relative sm:flex-row flex-col">
+          <div className="" />
+          {platformDownloads.map((item) => (
+            <DownloadButton key={item.platform} platform={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
