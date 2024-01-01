4ca906fb4d8d6548ef012309d65dc17eea7cbb4f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-01 13:05:14 -0600

    Fix build

--- apps/desktop/src/components/nav-bar.tsx
+++ apps/desktop/src/components/nav-bar.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Settings, Pin, Download, ArrowLeftToLine, ArrowRightToLine, ChevronsRightLeft } from "lucide-react";
 
 import React from "react";
