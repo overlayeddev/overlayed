16374d9bd394cbbf2f7140c108a8b8a0dc109b28
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-03 11:53:08 -0600

    Particles (#23)
    Co-authored-by: Nick C <niemal@users.noreply.github.com>

--- apps/web/src/components/react/download.tsx
+++ apps/web/src/components/react/download.tsx
@@ -20,12 +20,30 @@ export const Download = () => {
   >([]);
 
   useEffect(() => {
-    fetch(`${API_HOST}/latest`)
-      .then((res) => res.json())
-      .then((res) => {
-        setPlatformDownloads(res);
-        setIsLoading(false);
-      });
+    // fetch(`${API_HOST}/latest`)
+    //   .then((res) => res.json())
+    //   .then((res) => {
+    //     setPlatformDownloads(res);
+    //     setIsLoading(false);
+    //   });
+    setIsLoading(false);
+    setPlatformDownloads([
+      {
+        name: "linux",
+        url: "https://something",
+        platform: "linux",
+      },
+      {
+        name: "linux",
+        url: "https://something",
+        platform: "mac",
+      },
+      {
+        name: "linux",
+        url: "https://something",
+        platform: "windows",
+      },
+    ]);
   }, []);
 
   return (
