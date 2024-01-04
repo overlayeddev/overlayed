eaffa39130616ce37f5c5ee9f21a79743d2f3dd6
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-03 21:51:16 -0600

    More work!

--- apps/desktop/src/views/settings/account.tsx
+++ apps/desktop/src/views/settings/account.tsx
@@ -1,4 +1,3 @@
-
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { exit } from "@tauri-apps/api/process";
@@ -33,7 +32,7 @@ export const Account = () => {
   }, []);
   return (
     <>
-      <div className="">
+      <div>
         {me?.id ? (
           <p className="mb-3 font-bold">
             {me?.global_name} ({me?.id})
@@ -49,88 +48,93 @@ export const Account = () => {
             </p>
           )}
         </div>
-        <div className="flex gap-2">
-          {me?.id ? (
-            <Dialog
-              onOpenChange={e => {
-                setShowLogoutDialog(e);
-              }}
-              open={showLogoutDialog}
-            >
-              <DialogTrigger asChild>
-                <Button disabled={!me?.id}>Logout</Button>
-              </DialogTrigger>
-              <DialogContent className="w-[80%]">
-                <form
-                  onSubmit={event => {
-                    event.preventDefault();
-                    setShowLogoutDialog(false);
-                    setMe(null);
-                    localStorage.removeItem("discord_access_token");
-                    localStorage.removeItem("discord_expires_at");
-                    navigate("/");
-                  }}
-                >
-                  <DialogHeader>
-                    <DialogTitle className="text-xl mb-4 text-white">Logout</DialogTitle>
-                    <DialogDescription className="text-xl mb-4 text-white">
-                      Are you sure you want to log out of Overlayed?
-                    </DialogDescription>
-                  </DialogHeader>
-                  <DialogFooter>
-                    <DialogClose asChild>
-                      <Button variant="secondary">Cancel</Button>
-                    </DialogClose>
-                    <Button type="submit">Confirm Logout</Button>
-                  </DialogFooter>
-                </form>
-              </DialogContent>
-            </Dialog>
-          ) : (
-            <Button variant="default">
-              <Link to="/" className="no-underline text-white hover:text-white">
-                Login to Discord
-              </Link>
-            </Button>
-          )}
-        </div>
-      </div>
 
-      <div>
-        <Dialog
-          onOpenChange={e => {
-            setShowQuitDialog(e);
-          }}
-          open={showQuitDialog}
-        >
-          <DialogTrigger asChild>
-            <Button>Quit Overlayed</Button>
-          </DialogTrigger>
-          <DialogContent className="w-[80%]">
-            <form
-              onSubmit={async event => {
-                event.preventDefault();
-                await saveWindowState(StateFlags.ALL);
-                await exit();
-              }}
-            >
-              <DialogHeader>
-                <DialogTitle className="text-xl mb-4 text-white">Quit Overlayed</DialogTitle>
-                <DialogDescription className="text-xl mb-4 text-white">
-                  Are you sure you want to quit the Overlayed app?
-                </DialogDescription>
-              </DialogHeader>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="secondary">Cancel</Button>
-                </DialogClose>
-                <Button variant="destructive" type="submit">
-                  Quit
-                </Button>
-              </DialogFooter>
-            </form>
-          </DialogContent>
-        </Dialog>
+        <div className="flex flex-col gap-4 pb-4">
+          <div>
+            {me?.id ? (
+              <Dialog
+                onOpenChange={e => {
+                  setShowLogoutDialog(e);
+                }}
+                open={showLogoutDialog}
+              >
+                <DialogTrigger asChild>
+                  <Button disabled={!me?.id} className="w-[100px]">
+                    Logout
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="w-[80%]">
+                  <form
+                    onSubmit={event => {
+                      event.preventDefault();
+                      setShowLogoutDialog(false);
+                      setMe(null);
+                      localStorage.removeItem("discord_access_token");
+                      localStorage.removeItem("discord_expires_at");
+                      navigate("/");
+                    }}
+                  >
+                    <DialogHeader>
+                      <DialogTitle className="text-xl mb-4 text-white">Logout</DialogTitle>
+                      <DialogDescription className="text-xl mb-4 text-white">
+                        Are you sure you want to log out of Overlayed?
+                      </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="secondary">Cancel</Button>
+                      </DialogClose>
+                      <Button type="submit">Confirm Logout</Button>
+                    </DialogFooter>
+                  </form>
+                </DialogContent>
+              </Dialog>
+            ) : (
+              <Button variant="default">
+                <Link to="/" className="no-underline text-white hover:text-white">
+                  Login to Discord
+                </Link>
+              </Button>
+            )}
+          </div>
+        </div>
+
+        <div>
+          <Dialog
+            onOpenChange={e => {
+              setShowQuitDialog(e);
+            }}
+            open={showQuitDialog}
+          >
+            <DialogTrigger asChild>
+              <Button className="w-[100px]">Quit</Button>
+            </DialogTrigger>
+            <DialogContent className="w-[80%]">
+              <form
+                onSubmit={async event => {
+                  event.preventDefault();
+                  await saveWindowState(StateFlags.ALL);
+                  await exit();
+                }}
+              >
+                <DialogHeader>
+                  <DialogTitle className="text-xl mb-4 text-white">Quit Overlayed</DialogTitle>
+                  <DialogDescription className="text-xl mb-4 text-white">
+                    Are you sure you want to quit the Overlayed app?
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button variant="secondary">Cancel</Button>
+                  </DialogClose>
+                  <Button variant="destructive" type="submit">
+                    Quit
+                  </Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
     </>
   );
