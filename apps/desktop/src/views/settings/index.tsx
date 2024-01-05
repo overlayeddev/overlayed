0750eb9791cbd0945c30c6ed701d13830a1b00f5
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-04 23:17:59 -0600

    zzz update banner is scuffed

--- apps/desktop/src/views/settings/index.tsx
+++ apps/desktop/src/views/settings/index.tsx
@@ -9,8 +9,20 @@ export const SettingsView = ({
 }: {
   update: { isAvailable: boolean; status: UpdateStatus | null; error: string };
 }) => {
+  const search = window.location.hash.substring(1);
+  const params = new URLSearchParams(search);
+
+  const shouldShowUpdate = update.isAvailable;
+
   return (
     <div className="bg-zinc-900 w-[calc(100vw)] h-full">
+      <pre>
+        {JSON.stringify(
+          { params, all: params.entries(), search },
+          null,
+          2
+        )}
+      </pre>
       <Tabs defaultValue="account" className="w-full">
         <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="account">General</TabsTrigger>
@@ -18,6 +30,7 @@ export const SettingsView = ({
           <TabsTrigger value="developer">Developer</TabsTrigger>
         </TabsList>
 
+        {shouldShowUpdate && <Updater update={update} />}
         <div className="p-4 pt-4 pb-14 overflow-auto">
           <div className="flex flex-col gap-4">
             <TabsContent value="account">
@@ -30,7 +43,6 @@ export const SettingsView = ({
           </div>
         </div>
       </Tabs>
-      {update.isAvailable && <Updater update={update} />}
     </div>
   );
 };
