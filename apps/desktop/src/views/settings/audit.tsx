5cb49c9614ab7bd2485459b6e0b5d58a900be4d4
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-29 18:55:09 -0600

    Move audit log into settings

---
+++ apps/desktop/src/views/settings/audit.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { useAppStore } from "@/store";
+import { Eraser, PhoneOff, PhoneIncoming } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export const Audit = () => {
+  const { userLog, resetUserLog } = useAppStore();
+
+  return (
+    <div className="flex flex-col text-center h-screen">
+      <div className="flex items-center pb-4">
+        <p className="text-2xl">Audit Log</p>
+        <Button onClick={resetUserLog} variant="ghost" size="sm" className="w-20">
+          <Eraser size={18} />
+        </Button>
+      </div>
+
+      <div className="overflow-auto">
+        {[...userLog].reverse().map((user, i) => {
+          const Icon = user.event === "join" ? PhoneIncoming : PhoneOff;
+          const className = user.event === "join" ? "text-green-500" : "text-red-500";
+          return (
+            <div key={i} className="text-lg flex items-center text-white">
+              <Icon size={18} className={cn(className, "mr-2")} /> <span>{user.username}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
