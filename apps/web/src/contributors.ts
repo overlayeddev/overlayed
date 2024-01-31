8a531fd56376e7e01293300cc111b1c016ad0916
Hacksore <Hacksore@users.noreply.github.com>
2024-01-31 00:07:03 +0000

    Update contributors

--- apps/web/src/contributors.ts
+++ apps/web/src/contributors.ts
@@ -20,7 +20,7 @@ export const contributors = [
         "received_events_url": "https://api.github.com/users/Hacksore/received_events",
         "type": "User",
         "site_admin": false,
-        "contributions": 482
+        "contributions": 485
     },
     {
         "login": "maxdemaio",
