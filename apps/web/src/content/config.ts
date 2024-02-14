247a1cb8f698eb8cf00d7aa54f90ffcbb200ca13
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-02-14 10:48:50 -0600

    Fix SEO for canonical url

--- apps/web/src/content/config.ts
+++ apps/web/src/content/config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     customSlug: z.string(),
+    canonicalPath: z.string().optional(),
     pubDate: z.string(),
     description: z.string(),
     draft: z.boolean().optional(),
