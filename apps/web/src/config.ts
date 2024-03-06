600a403ae9e427dc59d8307b4af6a5d48d3895bd
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-06 07:26:16 -0600

    Update OG

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -2,7 +2,7 @@ export const SITE_TITLE =
   "Overlayed - Discord overlay for Mac, Windows, and Linux";
 export const SITE_DESCRIPTION =
   "A modern, open-source, and free voice chat overlay for Discord. Overlayed supports Mac, Windows, and Linux!";
-export const SITE_IMAGE = "/hero.png";
+export const SITE_IMAGE = "/og.png";
 export const DISCORD_INVITE_LINK = "https://discord.gg/eXmeNkVjye";
 
 export const API_HOST =
