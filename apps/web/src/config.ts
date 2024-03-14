d7a07f9139b1ecd9517fda5eaa57d93705cf8b44
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-14 09:31:04 -0500

    Update SEO

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE =
   "Overlayed - Discord overlay for Mac, Windows, and Linux";
 export const SITE_DESCRIPTION =
-  "A modern, open-source, and free voice chat overlay for Discord. Overlayed supports Mac, Windows, and Linux!";
+  "Overlayed lets you see who's speaking in any voice channel with a movable overlay, compatible with almost every game or app";
 export const SITE_IMAGE = "/og.png";
 export const DISCORD_INVITE_LINK = "https://discord.gg/eXmeNkVjye";
 
