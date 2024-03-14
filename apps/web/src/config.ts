75904529e2e53456d1f66773f0c39c700e98d605
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-14 09:42:25 -0500

    Add discord keyword

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE =
   "Overlayed - Discord overlay for Mac, Windows, and Linux";
 export const SITE_DESCRIPTION =
-  "Overlayed lets you see who's speaking in any voice channel with a movable overlay, compatible with almost every game or app";
+  "Overlayed lets you see who's speaking in any Discord voice channel with a movable overlay, compatible with almost every game or app";
 export const SITE_IMAGE = "/og.png";
 export const DISCORD_INVITE_LINK = "https://discord.gg/eXmeNkVjye";
 
