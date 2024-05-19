6178b396537593e5c1d10ee9d0c0332295acd192
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-19 09:27:37 -0500

    Progress on desktop

--- apps/desktop/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: [require.resolve("lint/vite-react")],
-  rules: {
-    // NOTE: this is an open issue https://github.com/shadcn-ui/ui/issues/120
-    // most are just disabling the error for now
-    "react/prop-types": [2, { ignore: ["className"] }],
-  },
-};
--- apps/desktop/src/views/channel.tsx
+++ apps/desktop/src/views/channel.tsx
@@ -9,12 +9,12 @@ export const ChannelView = ({ alignDirection }: { alignDirection: DirectionLR })
   const { value: showOnlyTalkingUsers } = useConfigValue<boolean>("showOnlyTalkingUsers");
 
   const allUsers = Object.entries(users);
-  const userList = showOnlyTalkingUsers ? allUsers.filter(([_, item]) => item.talking || item.id === me.id) : allUsers;
+  const userList = showOnlyTalkingUsers ? allUsers.filter(([, item]) => item.talking || item.id === me.id) : allUsers;
 
   return (
     <div>
       <div className={`py-2 ${alignDirection == "center" ? "flex flex-wrap justify-center" : ""}`}>
-        {userList.map(([_, item]) => (
+        {userList.map(([, item]) => (
           <User key={item.id} item={item} alignDirection={alignDirection} />
         ))}
       </div>
