64fb505e17e116eedd82fb3e024f426a1a3a696f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-05-23 08:27:41 -0500

    Upload nightly bins to r2

---
+++ scripts/actions/upload-to-r2.js
@@ -0,0 +1,7 @@
+// TODO: upload the nightly bins to r2
+
+/** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+export const script = async ({ context, github }, paths) => {
+  console.log("TODO: impl this");
+  console.log("Paths:", paths);
+};
