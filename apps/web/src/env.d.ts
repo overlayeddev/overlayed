0c7b65bbf3e3d89054668201919a600bf6c340b2
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-01 21:02:15 -0500

    Fullscreen blog (#81)

--- apps/web/src/env.d.ts
+++ apps/web/src/env.d.ts
@@ -1,3 +1,4 @@
-import "../.astro/types.d.ts";
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
