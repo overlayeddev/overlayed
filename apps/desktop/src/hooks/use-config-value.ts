acc3274557ed17e29a5462bbb0888b439d18f4a6
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-08 16:26:57 -0500

    Moar progress

--- apps/desktop/src/hooks/use-config-value.ts
+++ apps/desktop/src/hooks/use-config-value.ts
@@ -2,18 +2,27 @@ import Config, { type OverlayedConfig, type OverlayedConfigKey } from "@/config"
 import { listen } from "@tauri-apps/api/event";
 import { useEffect, useState } from "react";
 
-export const useConfigValue = <T>(
-  key: OverlayedConfigKey
+export const useConfigValue = <T extends OverlayedConfigKey>(
+  key: T
 ): {
-  value: T;
+  value: OverlayedConfig[T];
 } => {
-  const [value, setValue] = useState(Config.get(key));
+  const [value, setValue] = useState<OverlayedConfig>();
 
   useEffect(() => {
+
+    const init = () =>{
+      Config.get(key).then(setValue)
+    }
+
+    init();
+
     const listenFn = listen<OverlayedConfig>("config_update", event => {
       const { payload } = event;
 
       // update the latest value
+      // TODO: fix
+      // @ts-expect-error idk
       setValue(payload[key]);
     });
 
