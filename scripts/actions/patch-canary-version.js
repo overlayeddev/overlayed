714d8bf73c4570b9ff5f64580ede85f656c00033
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-17 17:15:30 -0500

    Fix windows canary build

--- scripts/actions/patch-canary-version.js
+++ scripts/actions/patch-canary-version.js
@@ -1,12 +1,16 @@
 import fs from "node:fs";
+import path from "node:path";
 
 /** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
 export const script = async ({ context, github }) => {
   // get the current commit sha
   const commitSha = context?.sha || "dev";
 
+  // make the file path work on all platforms
+  const tauriConfig = path.join("apps", "desktop", "src-tauri", "tauri.conf.canary.json");
+
   // read the apps/desktop/src-tauri/tauri.conf.canary.json
-  const currentFileJson = JSON.parse(fs.readFileSync("apps/desktop/src-tauri/tauri.conf.canary.json", "utf8"));
+  const currentFileJson = JSON.parse(fs.readFileSync(tauriConfig, "utf8"));
 
   // patched version
   const patchVersionName = `0.0.0-canary.${commitSha.slice(0, 7)}`;
@@ -15,5 +19,5 @@ export const script = async ({ context, github }) => {
   currentFileJson["package"]["version"] = patchVersionName;
 
   // write it back to the file
-  fs.writeFileSync("apps/desktop/src-tauri/tauri.conf.canary.json", JSON.stringify(currentFileJson, null, 2));
+  fs.writeFileSync(tauriConfig, JSON.stringify(currentFileJson, null, 2));
 };
