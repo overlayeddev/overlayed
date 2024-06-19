e388ff8406efc7e5de820d6613a820d379b07bbc
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-18 19:27:55 -0500

    No way

--- apps/web/generate-licenses.js
+++ apps/web/generate-licenses.js
@@ -3,7 +3,7 @@ import path from "node:path";
 import { execSync } from "child_process";
 console.log("Attempting to generate licenses...");
 
-// Pls 🙏 work
+// Pls 🙏 work no way
 try {
   // root monorepo dir
   const __dirname = path.dirname(new URL(import.meta.url).pathname);
