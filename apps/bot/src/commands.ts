75feb9462523147ecc7bcf501e98953d9f99bbf0
jean-philippe <jp@zelexio.com>
2024-07-09 16:08:51 -0400

    added uninstall  command

--- apps/bot/src/commands.ts
+++ apps/bot/src/commands.ts
@@ -2,3 +2,7 @@ export const INSTALL = {
 	name: "install",
 	description: "How to install the Overlayed desktop app",
 };
+export const UNINSTALL = {
+	name: "uninstall",
+	description: "How to uninstall the Overlayed desktop app",
+};
