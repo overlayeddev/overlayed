3802d8a9aa42a0721072be6c9c989f12e2999b46
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-11 22:45:48 -0500

    Allow zoom

--- apps/desktop/src-tauri/src/commands.rs
+++ apps/desktop/src-tauri/src/commands.rs
@@ -5,6 +5,30 @@ use tauri::{Manager, State, SystemTrayHandle, Window};
 use crate::{constants::*, Pinned};
 
 #[tauri::command]
+pub fn zoom_window(window: tauri::Window, scale_factor: f64) {
+  let _ = window.with_webview(move |webview| {
+    #[cfg(target_os = "linux")]
+    {
+      // see https://docs.rs/webkit2gtk/0.18.2/webkit2gtk/struct.WebView.html
+      // and https://docs.rs/webkit2gtk/0.18.2/webkit2gtk/trait.WebViewExt.html
+      use webkit2gtk::traits::WebViewExt;
+      webview.inner().set_zoom_level(scale_factor);
+    }
+
+    #[cfg(windows)]
+    unsafe {
+      // see https://docs.rs/webview2-com/0.19.1/webview2_com/Microsoft/Web/WebView2/Win32/struct.ICoreWebView2Controller.html
+      webview.controller().SetZoomFactor(scale_factor).unwrap();
+    }
+
+    #[cfg(target_os = "macos")]
+    unsafe {
+      let () = msg_send![webview.inner(), setPageZoom: scale_factor];
+    }
+  });
+}
+
+#[tauri::command]
 pub fn open_settings(window: Window, update: bool) {
   let app = window.app_handle();
   let settings_windows = app.get_window(SETTINGS_WINDOW_NAME);
