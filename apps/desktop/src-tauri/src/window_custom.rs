7e8cd297061a298d7fc89bc42fce9dcd5c59f989
Victor Aremu <me@victorare.mu>
2024-07-19 22:02:45 +0100

    feat: implement set_inactive_on_mouse_hover

--- apps/desktop/src-tauri/src/window_custom.rs
+++ apps/desktop/src-tauri/src/window_custom.rs
@@ -44,15 +44,64 @@ pub mod macos {
   }
 }
 
-use tauri::{Runtime, Window};
+use tauri::{Manager, Runtime, Window};
+
+use crate::{get_pin, Pinned};
 
 pub trait WindowExt {
   fn set_document_title(&self, url: &str);
+
+  fn set_inactive_on_mouse_hover(&self);
 }
 
 impl<R: Runtime> WindowExt for Window<R> {
   fn set_document_title(&self, title: &str) {
     let str = format!("document.title = '{:}'", title);
     self.eval(&str).unwrap();
+  }
+
+  fn set_inactive_on_mouse_hover(&self) {
+    let window = self.clone();
+
+    self.listen_global("mouse-move", move |event| {
+      let is_pinned = get_pin(window.state::<Pinned>());
+
+      if !is_pinned {
+        return;
+      }
+
+      let (x, y): (f64, f64) = serde_json::from_str(event.payload().unwrap()).unwrap();
+
+      let scale_factor = window.scale_factor().unwrap();
+
+      let window_position = window
+        .inner_position()
+        .unwrap()
+        .to_logical::<f64>(scale_factor);
+
+      let window_size = window.inner_size().unwrap().to_logical::<f64>(scale_factor);
+
+      let intersects = {
+        let window_x = window_position.x;
+        let window_y = window_position.y;
+        let window_width = window_size.width;
+        let window_height = window_size.height;
+
+        x >= window_x
+          && x <= window_x + window_width
+          && y >= window_y
+          && y <= window_y + window_height
+      };
+
+      let is_visible = window.is_visible().unwrap();
+
+      if intersects && is_visible {
+        window.hide().unwrap();
+      }
+
+      if !intersects && !is_visible {
+        window.show().unwrap();
+      }
+    });
   }
 }
