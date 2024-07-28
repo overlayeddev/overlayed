f772b15fba70ae9e5651584404df46513abbcb2d
CowboySticky <ncurtin09@outlook.com>
2024-07-27 21:16:36 -0500

    This ensures that the current user is shown correc -tly based on the configuration settings.

--- apps/desktop/src/views/channel.tsx
+++ apps/desktop/src/views/channel.tsx
@@ -12,13 +12,16 @@ export const ChannelView = ({ alignDirection }: { alignDirection: DirectionLR })
   const allUsers = Object.entries(users);
   let userList = showOnlyTalkingUsers ? allUsers.filter(([, item]) => item.talking) : allUsers;
 
-  if (!showOwnUser) {
-    userList = userList.filter(([, item]) => item.id !== me?.id);
-  }
+  userList = userList.filter(([, item]) => {
+    if (item.id === me?.id) {
+      return showOwnUser && (showOnlyTalkingUsers ? item.talking : true);
+    }
+    return true;
+  });
 
   return (
     <div>
-      <div className={`py-2 ${alignDirection == "center" ? "flex flex-wrap justify-center" : ""}`}>
+      <div className={`py-2 ${alignDirection === "center" ? "flex flex-wrap justify-center" : ""}`}>
         {userList.map(([, item]) => (
           <User key={item.id} item={item} alignDirection={alignDirection} />
         ))}
