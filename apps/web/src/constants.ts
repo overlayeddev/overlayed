d18e1225eb89ca362ffb030fc6cdbd0d77a52c69
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-07-30 14:25:56 -0500

    Update constants.ts

--- apps/web/src/constants.ts
+++ apps/web/src/constants.ts
@@ -4,7 +4,7 @@ export const SITE_DESCRIPTION =
   "See who's speaking in your Discord voice channel with a movable overlay, compatible with almost every game or app";
 export const SITE_IMAGE = "/og.png";
 export const DISCORD_INVITE_LINK = "https://discord.gg/eXmeNkVjye";
-export const GITHUB_URL = "https://github.com/overlayedev/overlayed";
+export const GITHUB_URL = "https://github.com/overlayeddev/overlayed";
 
 export const API_HOST =
   process.env.NODE_ENV === "production" ? "https://update.overlayed.dev" : "";
