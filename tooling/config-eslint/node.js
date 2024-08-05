232a9f33aaea3fec7a7c9147305a7fae10df8a9e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-08-05 07:58:33 -0500

    Fix eslint

--- tooling/config-eslint/node.js
+++ tooling/config-eslint/node.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
 
 const config = tseslint.config(
   js.configs.recommended,
@@ -10,6 +11,14 @@ const config = tseslint.config(
     ignores: ["node_modules/", "dist/"],
   },
   {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        process: true
+      },
+    },
     rules: {
       quotes: ["error", "double"],
       "no-unused-vars": "off",
