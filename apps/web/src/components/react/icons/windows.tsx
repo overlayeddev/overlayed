aa48f9a1040c682e021230fa5fa5addc090f1566
Mehdi <pyrexrj@gmail.com>
2024-08-07 22:20:47 +0330

    fix: fill values

--- apps/web/src/components/react/icons/windows.tsx
+++ apps/web/src/components/react/icons/windows.tsx
@@ -3,6 +3,7 @@ function Windows({ className }: { className?: string }) {
     <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path
         className={className}
+        fill="currentColor"
         d="M0,0H11.377V11.372H0ZM12.623,0H24V11.372H12.623ZM0,12.623H11.377V24H0Zm12.623,0H24V24H12.623"
       />
     </svg>
