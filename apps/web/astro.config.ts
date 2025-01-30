10ffd5c27c324bf471872740ce614c1957a1f623
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-29 19:56:46 -0600

    Update all sub package deps (#236)

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
