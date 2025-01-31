1b927179e4166ba830bb276816a73aaf2a995d59
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-01-30 21:20:31 -0600

    tailwind :shipit:

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import proxyMiddleware from "./plugins/proxy-middleware.js";
 import { targetBlank } from "./plugins/target-blank.js";
+import tailwindcss from "@tailwindcss/vite";
 
-const integrations = [sitemap(), react(), tailwind(), mdx()];
+const integrations = [sitemap(), react(), mdx()];
 
 if (process.env.NODE_ENV !== "production") {
   integrations.push(
@@ -24,17 +23,24 @@ export default defineConfig({
   site: "https://overlayed.dev",
   trailingSlash: "never",
   integrations,
+
   server: {
     host: "0.0.0.0",
     // NOTE: using this port cause discord had it set for streamkit RPC Origin of http://localhost:3000
     port: 3000,
   },
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+
   markdown: {
     rehypePlugins: [targetBlank],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
