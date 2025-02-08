cdff8742043ca2455018fa837ce221596e14f5d3
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-02-08 13:08:41 -0600

    Fix

--- apps/desktop/src/hooks/use-set-size.ts
+++ apps/desktop/src/hooks/use-set-size.ts
@@ -1,17 +1,17 @@
-import { LogicalSize } from "@tauri-apps/api/dpi";
+import { PhysicalSize } from "@tauri-apps/api/dpi";
 import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
 import { useEffect, useRef } from "react";
 const appWindow = getCurrentWebviewWindow();
 
 export const useSetWindowSize = ({ width, height }: { width: number; height: number }) => {
-  const lastSizeRef = useRef<LogicalSize | null>(null);
+  const lastSizeRef = useRef<PhysicalSize | null>(null);
 
   useEffect(() => {
     appWindow.outerSize().then(size => {
       lastSizeRef.current = size;
 
       // set the new
-      appWindow.setSize(new LogicalSize(width, height));
+      appWindow.setSize(new PhysicalSize(width, height));
     });
 
     // on unmount save the last size
